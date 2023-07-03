3e3840b28a1edb37eab78f65a6fa5ea19ef026e7
Kirill1800 <74734205+Kirill1800@users.noreply.github.com>
2023-07-03 20:06:42 +0200

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,13 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        for (int i = 1; i < 10; i++) {
+            for (int j = 0; j < i; j++) {
+                System.out.print("*");
+            }
+            System.out.println(" ");
+        }
+    }
+}

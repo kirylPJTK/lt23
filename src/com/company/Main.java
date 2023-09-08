193b3590ea2181fcfeaee8124b73c72cde501103
Kirill1800 <74734205+Kirill1800@users.noreply.github.com>
2023-09-08 19:26:19 +0200

    powtorka przed studiami

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,13 +1,17 @@
 package com.company;
 
 public class Main {
-
     public static void main(String[] args) {
-        for (int i = 1; i < 10; i++) {
-            for (int j = 0; j < i; j++) {
-                System.out.print("*");
-            }
-            System.out.println(" ");
+//        for (int i = 0; i < 10; i++) {
+//            for (int j = 0; j < i; j++) {
+//                System.out.print("*");
+//            }
+//            System.out.println(" ");
+//        }
+        for (int i = 75; i <= 150; i++) {
+            System.out.println(i + " - " + (char)i);
         }
+
+
     }
 }

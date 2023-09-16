aa15b5d026e52e440bb7f87d5f4db14f2352385a
Kirill1800 <74734205+Kirill1800@users.noreply.github.com>
2023-09-16 22:05:05 +0200

    Polimorfizm i rekursja w Java

--- src/Powtutka/Restourant/Restaurant.java
+++ src/Powtutka/Restourant/Restaurant.java
@@ -3,7 +3,7 @@ package Powtutka.Restourant;
 public class Restaurant {
     public static void main(String[] args) {
         Dish pizza = new Pizza("Pizza Peperoni", 12);
-        Dish salad = new Pizza("Olivje Nachuj", 32);
+        Dish salad = new Salad("Olivje Nachuj", 32);
         Dish beverage = new Beverage("Tea", 2, true);
 
 

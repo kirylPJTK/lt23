44c486c54cc3e7b6e4d7acd180481ec75c96e5e3
Kirill1800 <74734205+Kirill1800@users.noreply.github.com>
2023-10-16 11:35:40 +0200

    To Do list with write and read files

--- src/Junior_java_develop/TASK4/WriteFile.java
+++ src/Junior_java_develop/TASK4/WriteFile.java
@@ -7,13 +7,7 @@ public class WriteFile {
     public static void main(String[] args) {
 
         try(FileWriter writer = new FileWriter("text.txt", false)) {
-            String text = "Hello Gold!";
-            writer.write(text);
-            writer.append('\n');
-            writer.append('E');
-            writer.append('\n');
-            writer.append(text);
-            writer.append("\nHello Kirill");
+            writer.append("Some info");
 
             writer.flush();
         } catch (IOException e) {

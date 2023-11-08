ff14fbba891ac0207f9b886559933d2b94c99742
Kirill1800 <74734205+Kirill1800@users.noreply.github.com>
2023-11-08 19:14:58 +0100

    Packing

---
+++ src/Junior_java_develop/TASK6/NO2/Packing.java
@@ -0,0 +1,12 @@
+package Junior_java_develop.TASK6.NO2;
+
+public class Packing {
+    public static void main(String[] args) {
+        Integer ob1 = m(100);
+
+        System.out.println(ob1);
+    }
+    static int m(Integer ob) {
+        return ob;
+    }
+}

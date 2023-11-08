9ee89fde6504966e39b83a19d26eb2cf4197c862
Kirill1800 <74734205+Kirill1800@users.noreply.github.com>
2023-11-08 17:06:32 +0100

    Enum

--- src/Junior_java_develop/TASK6/NO1/Apple.java
+++ src/Junior_java_develop/TASK6/NO1/Apple.java
@@ -1,5 +1,15 @@
 package Junior_java_develop.TASK6.NO1;
 
 public enum Apple {
-    Jonathan, GoldenDel, RedDel, Winesap, Cortland
+    Jonathan(10), GoldenDel(9), RedDel(8), Winesap(7), Cortland(6);
+
+    private int price;
+
+    Apple(int p) {
+        this.price = p;
+    }
+
+    public int getPrice() {
+        return price;
+    }
 }
--- src/Junior_java_develop/TASK6/NO1/EnumDemo.java
+++ src/Junior_java_develop/TASK6/NO1/EnumDemo.java
@@ -14,5 +14,64 @@ public class EnumDemo {
         if (ap == Apple.GoldenDel)
             System.out.println("ap include GoldenDel.\n");
 
+        Apple apples[] = Apple.values();
+
+        for (Apple a : apples) {
+            System.out.println(a.getPrice());
+        }
+
+        ap = Apple.valueOf("Winesap");
+        System.out.println("ap = " + ap);
+
+        System.out.println(Apple.Jonathan.ordinal());
+
+       Apple ap1, ap2, ap3;
+
+       for (Apple a : Apple.values()) {
+           System.out.println(a + " " + a.ordinal());
+       }
+
+       ap1 = Apple.RedDel;
+       ap2 = Apple.GoldenDel;
+       ap3 = Apple.RedDel;
+
+        System.out.println(
+
+        );
+
+        if (ap1.compareTo(ap2) < 0)
+            System.out.println("ap1 < ap2");
+
+        if (ap1.compareTo(ap2) > 0)
+            System.out.println("ap1 > ap2");
+        if(ap1.compareTo(ap3) == 0)
+            System.out.println(ap1 + " equals " + ap3);
+        if (ap1.equals (ap2) )
+            System . out . println ( "Error ! " ) ;
+        if (ap1.equals (ap3) )
+            System.out.println(ap1 + " equals " + ap3);
+        if(ap1 == ap3)
+            System.out.println((ap1 + " == " + ap3));
+
+
+
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+

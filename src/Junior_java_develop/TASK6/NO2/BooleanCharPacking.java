eb5ef85a305375fbabe409b5d3b462b6a01fbe71
Kirill1800 <74734205+Kirill1800@users.noreply.github.com>
2023-11-12 23:17:49 +0100

    Boolean and Char packing!

---
+++ src/Junior_java_develop/TASK6/NO2/BooleanCharPacking.java
@@ -0,0 +1,16 @@
+package Junior_java_develop.TASK6.NO2;
+
+public class BooleanCharPacking {
+    public static void main(String[] args) {
+        Boolean b = true;
+        if (b) {
+            System.out.println("b == true");
+        }
+
+        Character ch = 'x';
+
+        char ch2 = ch;
+
+        System.out.println("ch2 == " + ch);
+    }
+}
